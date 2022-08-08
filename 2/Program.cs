827a21d9007e6ad3f488b04aed41b2d8f1a82cad
Chrtumanovich <chris13@tut.by>
2022-08-08 17:55:42 +0300

    First commit

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Main(string[] args)
+{
+    string a;
+    Console.WriteLine("Введите первое число:");
+    a = Console.ReadLine();
+
+    string b;
+    Console.WriteLine("Введите второе число:");
+    b = Console.ReadLine();
+}
--- 3LessonApp/Program.cs
+++ 3LessonApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/*
+
 int variable = 13;
 int variable2 = 20;
 variable = variable + variable2;
@@ -8,14 +8,3 @@ variable2 = variable2 - variable;
 
 Console.WriteLine("Variable " + variable);
 Console.WriteLine("Variable2 " + variable2);
-*/
-
-
-string a;
-Console.WriteLine("Введите первое число:");
-a = Console.ReadLine();
-
-string b;
-Console.WriteLine("Введите второе число:");
-b = Console.ReadLine();
-
