9903fa33d6fff493a607cc1c999a73450b333ef4
Chrtumanovich <chris13@tut.by>
2022-08-08 16:12:51 +0300

    Добавьте файлы проекта.

---
+++ 3LessonApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+int variable = 13;
+int variable2 = 20;
+variable = variable + variable2;
+variable2 = variable2 - variable;
+
+Console.WriteLine("Variable " + variable);
+Console.WriteLine("Variable2 " + variable2);
+*/
+
+
+string a;
+Console.WriteLine("Введите первое число:");
+a = Console.ReadLine();
+
+string b;
+Console.WriteLine("Введите второе число:");
+b = Console.ReadLine();
+
